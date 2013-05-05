b0f833ab68307ac5475bc37c88281d8f01819442
Andrew Williams <andy@tensixtyone.com>
2013-05-05 19:12:29 +0100

    PEP8 the utils

--- app/timer/models/utils.py
+++ app/timer/models/utils.py
@@ -1,12 +1,13 @@
 from django.db.models.fields.related import SingleRelatedObjectDescriptor
 from django.db.models.query import QuerySet
 
+
 class InheritanceQuerySet(QuerySet):
     def select_subclasses(self, *subclasses):
         if not subclasses:
             subclasses = [o for o in dir(self.model)
-                          if isinstance(getattr(self.model, o), SingleRelatedObjectDescriptor)\
-                          and issubclass(getattr(self.model,o).related.model, self.model)]
+                          if isinstance(getattr(self.model, o), SingleRelatedObjectDescriptor) \
+                          and issubclass(getattr(self.model, o).related.model, self.model)]
         new_qs = self.select_related(*subclasses)
         new_qs.subclasses = subclasses
         return new_qs
